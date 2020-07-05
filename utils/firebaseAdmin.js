b717ccb7aa010b6613652d4a3f502efcf2956310
Adrian <adriandinca2008@yahoo.com>
2020-07-05 23:09:02 +0300

    refactor: Generate certificate using env variables

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -2,7 +2,11 @@ import firebaseAdmin from 'firebase-admin';
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.applicationDefault(),
+    credential: firebaseAdmin.credential.cert({
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      projectId: process.env.FIREBASE_PROJECT_ID.replace(/\\n/g, '\n'),
+    }),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
 }
