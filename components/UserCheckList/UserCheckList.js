38df806848b34fba64cc6fbeb8545c5ee378f2de
Adrian <adriandinca2008@yahoo.com>
2020-07-10 11:41:40 +0300

    feat: Add checkable user list

---
+++ components/UserCheckList/UserCheckList.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  ListItemSecondaryAction,
+  Checkbox,
+} from '@material-ui/core';
+
+import Image from '../Image';
+
+const UserCheckList = ({ users, checked, onCheck, ...otherProps }) => {
+  return (
+    <List {...otherProps}>
+      {users &&
+        users.map(user => {
+          const labelId = `user-${user.id}`;
+          return (
+            <ListItem key={user.id} button onClick={onCheck(user.id)}>
+              <ListItemAvatar>
+                <Avatar>
+                  {user && (
+                    <Image
+                      preview={user.avatar?.preview}
+                      sources={user.avatar?.sources}
+                      alt=""
+                      width={40}
+                      height={40}
+                    />
+                  )}
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                id={labelId}
+                primary={user.name}
+                secondary={user.email}
+              />
+              <ListItemSecondaryAction tabIndex={-1}>
+                <Checkbox
+                  edge="end"
+                  checked={checked.indexOf(user.id) !== -1}
+                  onChange={onCheck(user.id)}
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              </ListItemSecondaryAction>
+            </ListItem>
+          );
+        })}
+    </List>
+  );
+};
+
+export default UserCheckList;
